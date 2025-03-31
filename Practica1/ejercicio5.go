3973510de5b58082073f013e58000a28b1d57588
Pedro Vega <103451058+peython7@users.noreply.github.com>
2025-03-30 21:53:02 -0300

    Add files via upload

--- Practica1/ejercicio5.go
+++ Practica1/ejercicio5.go
@@ -9,19 +9,21 @@ func main() {
 	var num float64
 	fmt.Println("Ingrese un numero :")
 	fmt.Scan(&num)
-	//var infinito float64 = math.Inf(1)  INFINITO
-	//var menosInfinito float64= math.Inf(-1)  INFINITO NEGATIVO
-	if num == 0 {
-		fmt.Println("El numero es 0")
+
+	// switch para seleccionar la condición
+	switch {
+	case num == 0:
+		fmt.Println("El número es 0")
 		return
-	} else if num < -18 {
-		num = math.Abs(num) //valor absoluto
-	} else if (num >= -18) && (num <= -1) {
-		num = float64(math.Mod(num, 4)) //a num le doy valor de entero y luego al resultado float64
-	} else if (num >= 1) && (num < 20) {
-		num = math.Pow(num, 2) //Incrementa al cuadrado
-	} else {
+	case num < -18:
+		num = math.Abs(num) // valor absoluto
+	case num >= -18 && num <= -1:
+		num = math.Mod(num, 4) // modulo con flotante
+	case num >= 1 && num < 20:
+		num = math.Pow(num, 2) // cuadrado
+	default:
 		num = -num
 	}
-	fmt.Println("El numero resultante es: ", num)
+
+	fmt.Println("El numero resultante es:", num)
 }
