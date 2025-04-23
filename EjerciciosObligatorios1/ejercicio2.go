61dc5d3e6420dbc3712e18ce072ca2be7fb503bf
Pedro Vega <103451058+peython7@users.noreply.github.com>
2025-04-23 01:05:00 -0300

    Add files via upload

---
+++ EjerciciosObligatorios1/ejercicio2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func invertirPalabra(palabra string) string {
+	runas := []rune(palabra)
+	j := len(runas) - 1
+	i := 0
+	ok := false
+	for ok == false {
+		if i > j {
+			ok = true
+		} else {
+			runas[i], runas[j] = runas[j], runas[i]
+			i++
+			j--
+		}
+	}
+	return string(runas)
+}
+
+func main() {
+	frase := "Qué lindo día es hoy"
+	palabras := strings.Fields(frase)
+	var resultado strings.Builder
+	for i := 0; i < len(palabras); i++ {
+		if (i+1)%2 != 0 {
+			resultado.WriteString(invertirPalabra(palabras[i]) + " ")
+		} else {
+			resultado.WriteString(palabras[i] + " ")
+		}
+	}
+	fmt.Print(resultado.String())
+}
