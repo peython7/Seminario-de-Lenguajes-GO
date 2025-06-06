0a4af0fccf0e0ed137ac62b4ea2669209b1374b9
Pedro Vega <103451058+peython7@users.noreply.github.com>
2025-06-06 01:17:39 -0300

    Update ejercicio4.go

--- Practica3/ejercicio4/ejercicio4.go
+++ Practica3/ejercicio4/ejercicio4.go
@@ -4,21 +4,22 @@ import "fmt"
 
 func pxng(enviar, recibe chan string, mensajes chan string, str string) {
 	for i := 0; i < 5; i++ {
-		<-recibe
-		mensajes <- str
-		enviar <- "ok"
+		<-recibe //Espera permiso antes de imprimir
+		mensajes <- str //Envía el mensaje "PING" o "PONG"
+		enviar <- "ok" //Da el permiso al otro
 	}
 }
 
 func main() {
-	mensajes := make(chan string)
-	ping := make(chan string)
-	pong := make(chan string)
+	mensajes := make(chan string) // canal de datos "PING"/"PONG"
+	ping := make(chan string)  // canal para señal de permiso PING
+	pong := make(chan string)  // canal para señal de permiso PONG
 
+	//Creo las goroutines
 	go pxng(pong, ping, mensajes, "PING")
 	go pxng(ping, pong, mensajes, "PONG")
 
-	ping <- "start"
+	ping <- "start" //Inicializa: da el primer permiso a PING
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-mensajes)
